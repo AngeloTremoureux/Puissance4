67985f28df66e69c88d9bc629e1112d711bf3673
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2021-11-21 13:43:31 +0100

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,21 @@ const path = require('path');
 module.exports = [
   {
     mode: 'none',
-    entry: './src/modules/Game',
+    entry: {
+      puissance4: [
+        './src/modules/Game',
+        './src/modules/CheckIfWinner',
+        './src/modules/gameManager',
+        './src/modules/Interfaces',
+        './src/modules/Jeton',
+        './src/modules/main',
+        './src/modules/MonTour',
+        './src/modules/RobotManager',
+        './src/modules/WinnerManager'
+      ],
+      tests: ['./src/modules/TestsUnits'],
+      util: ['./src/Utils']
+    },
     devtool: 'inline-source-map',
     module: {
       rules: [
@@ -17,139 +31,7 @@ module.exports = [
       extensions: [".ts", ".tsx", ".js"],
     },
     output: {
-      filename: 'Game.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/CheckIfWinner',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'CheckIfWinner.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/Jeton',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader'
-        },
-      ],
-    },
-    output: {
-      filename: 'Jeton.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/MonTour',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'MonTour.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/RobotManager',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'RobotManager.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/TestsUnits',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'TestsUnits.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/Utils',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'Utils.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/WinnerManager',
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'WinnerManager.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/gameManager',
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader'
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'gameManager.js',
-      path: path.resolve(__dirname, 'public/dist'),
-    }
-  },
-  {
-    mode: 'none',
-    entry: './src/modules/main',
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader'
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: 'main.js',
+      filename: '[name].js',
       path: path.resolve(__dirname, 'public/dist'),
     }
   }
