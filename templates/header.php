31f85a7af40287c5d6807b99500aa4c497400e58
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2021-11-21 11:46:04 +0100

    public mvc

--- templates/header.php
+++ templates/header.php
@@ -3,8 +3,8 @@
 <meta charset="utf-8">
 <meta name="description" content="Le puissance4 est un jeu de stratégie pour tout âge.  Le but du jeu est d'aligner 4 pions horizontalement, verticalement ou en diagonale.">
 
-<link rel="icon"       type="image/png" href="<?= PATH_TO_IMAGES ?>/pion_bleu.png">
-<link rel="stylesheet" type="text/css"  href="<?= PATH_TO_STYLES ?>/style.css">
+<link rel="icon"       type="image/png" href="/public/images/pion_bleu.png">
+<link rel="stylesheet" type="text/css"  href="/public/styles/style.css">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-ui@1.12.1/themes/base/all.css">
@@ -12,13 +12,13 @@
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" crossorigin="anonymous" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="></script>
 <script src="https://kit.fontawesome.com/f1e20245fc.js"      crossorigin="anonymous"></script>
 
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/CheckIfWinner.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/Game.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/Jeton.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/MonTour.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/RobotManager.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/TestsUnits.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/Utils.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/WinnerManager.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/main.js"></script>
-<script type="text/javascript" src="<?= PATH_TO_DIST ?>/gameManager.js"></script>
+<script type="text/javascript" src="/public/dist/CheckIfWinner.js"></script>
+<script type="text/javascript" src="/public/dist/Game.js"></script>
+<script type="text/javascript" src="/public/dist/Jeton.js"></script>
+<script type="text/javascript" src="/public/dist/MonTour.js"></script>
+<script type="text/javascript" src="/public/dist/RobotManager.js"></script>
+<script type="text/javascript" src="/public/dist/TestsUnits.js"></script>
+<script type="text/javascript" src="/public/dist/Utils.js"></script>
+<script type="text/javascript" src="/public/dist/WinnerManager.js"></script>
+<script type="text/javascript" src="/public/dist/main.js"></script>
+<script type="text/javascript" src="/public/dist/gameManager.js"></script>
