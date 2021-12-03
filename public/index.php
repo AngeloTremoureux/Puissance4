999e64ba4b5c1ec877699587d0abb456e410c1ce
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2021-12-03 22:06:30 +0100

    PSR fix format

--- public/index.php
+++ public/index.php
@@ -6,12 +6,11 @@ require_once (__DIR__ . '/../classes/Game.php');
 try {
     $game = new Game();
     $game->setTemplateInk(PATH_TO_TEMPLATES . '/skeleton.php');
-    
+
     if (isset($_GET['tailleVerticale'], $_GET['tailleHorizontale'])) {
         $game->setCase($_GET['tailleHorizontale'], $_GET['tailleVerticale']);
     } else {
         $game->setCase(7, 5);
-        
     }
     require($game->getTemplateInk());
 
